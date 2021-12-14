e9dcfa6ab2ff52fc40b96684ac51c37a80483e38
tigerwang98 <470601193@qq.com>
2021-12-14 22:07:29 +0800

    OK

---
+++ test/dabao.py
@@ -0,0 +1,6 @@
+from redis import StrictRedis, ConnectionPool
+
+pool = ConnectionPool(host='localhost', port=6379, password='', db=0)
+r = StrictRedis(connection_pool=pool)
+r.set('name', 'Bob')
+print(r.get('name'))
